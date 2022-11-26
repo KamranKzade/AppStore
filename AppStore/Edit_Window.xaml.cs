92afaa579c1a5310b8a65050a8dfce0f6090d00d
Kamran <311.kamran@gmail.com>
2022-11-26 05:10:58 +0400

    ADD Edit view etmezden evvel

---
+++ AppStore/Edit_Window.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppStore
+{
+    /// <summary>
+    /// Interaction logic for Edit_Window.xaml
+    /// </summary>
+    public partial class Edit_Window : Window
+    {
+        public Edit_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
